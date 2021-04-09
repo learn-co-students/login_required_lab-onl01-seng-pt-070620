c50f85fd3de0251069d20b4b52822c5719963762
kinnari <kinnari1022@yahoo.com>
2021-04-08 22:24:21 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,13 @@
+class SecretsController < ApplicationController
+    before_action :require_logged_in
+ 
+
+def show
+    # if !current_user
+    #     redirect_to(controller: 'sessions',
+    #     action: 'new')
+    end
+end
+
+
+
